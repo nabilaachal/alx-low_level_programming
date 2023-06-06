714825d7b5a6d179aba35e0eb1b57bc5bf6511cc
nabilaachal <nabilaa.chal@hotmail.com>
2023-06-06 04:24:00 -0700

    loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -2,33 +2,31 @@
 
 /**
  * find_listint_loop - finds the loop in a linked list
- * @head: linked list to search for
- *
- * Return: address of the node where the loop starts, or NULL
+ * @head: pointer to head of list
+ * Return: address of node where loop starts
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *slow = head;
-    listint_t *fast = head;
+	listint_t *tortoise;
+	listint_t *hare;
 
-    if (!head)
-        return (NULL);
-
-    while (slow && fast && fast->next)
-    {
-        fast = fast->next->next;
-        slow = slow->next;
-        if (fast == slow)
-        {
-            slow = head;
-            while (slow != fast)
-            {
-                slow = slow->next;
-                fast = fast->next;
-            }
-            return (fast);
-        }
-    }
-
-    return (NULL);
+	if (!head)
+		return (NULL);
+	tortoise = hare = head;
+	while (hare->next && (hare->next)->next)
+	{
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			return (hare);
+		}
+	}
+	return (NULL);
 }
